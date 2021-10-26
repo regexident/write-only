e1b47535b1d57bae709aec673a3d44d2a322b5eb
Vincent Esche <regexident@gmail.com>
2021-10-26 03:01:04 +0200

    Add missing modulee documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+//! References/slices that provide write-access, but no read-access.
+//!
+//! # Examples
+//!
+//! Write-only reference:
+//! ```
+//! use write_only::{prelude::*, Put};
+//!
+//! fn write<T: Put<u8>>(slice: &mut T) {
+//!     slice.put(42u8);
+//! }
+//!
+//! let mut value: u8 = 0;
+//!
+//! let mut reference = WriteOnlyRef::from(&mut value);
+//! write(&mut reference);
+//!
+//! assert_eq!(value, 42);
+//!```
+//!
+//! Write-only slice:
+//! ```
+//! use write_only::{prelude::*, PutAt};
+//!
+//! fn write<T: PutAt<u8>>(slice: &mut T) {
+//!     slice.put_at(2, 42u8);
+//! }
+//!
+//! let mut values: Vec<u8> = (0..10).collect();
+//!
+//! let mut slice = WriteOnlySlice::from(&mut values[..]);
+//! write(&mut slice);
+//!
+//! assert_eq!(values[2], 42u8);
+//! ```
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 mod reference;
@@ -8,6 +44,7 @@ pub use slice::{
     PutAt, PutFromSliceAt, VolatileWriteOnlySlice, WriteAt, WriteFromSliceAt, WriteOnlySlice,
 };
 
+/// The crate's prelude.
 pub mod prelude {
     pub use crate::reference::{Put as _, VolatileWriteOnlyRef, Write as _, WriteOnlyRef};
     pub use crate::slice::{
