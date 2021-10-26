7e28b2da685a4391600d8e2a466c61576c34474f
Vincent Esche <regexident@gmail.com>
2021-10-26 02:35:08 +0200

    Add `WriteOnlySlice` & `VolatileWriteOnlySlice`

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 mod reference;
+mod slice;
+
 pub use reference::{Put, VolatileWriteOnlyRef, Write, WriteOnlyRef};
+pub use slice::{
+    PutAt, PutFromSliceAt, VolatileWriteOnlySlice, WriteAt, WriteFromSliceAt, WriteOnlySlice,
+};
 
 pub mod prelude {
     pub use crate::reference::{Put as _, VolatileWriteOnlyRef, Write as _, WriteOnlyRef};
+    pub use crate::slice::{
+        PutAt as _, PutFromSliceAt as _, VolatileWriteOnlySlice, WriteAt as _,
+        WriteFromSliceAt as _, WriteOnlySlice,
+    };
 }
