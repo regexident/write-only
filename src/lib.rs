d151ba8634bf20eefb87383420c6dd6f422ded21
Vincent Esche <regexident@gmail.com>
2021-10-26 02:33:12 +0200

    Add `WriteOnlyRef` & `VolatileWriteOnlyRef`

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
+mod reference;
+pub use reference::{Put, VolatileWriteOnlyRef, Write, WriteOnlyRef};
+
+pub mod prelude {
+    pub use crate::reference::{Put as _, VolatileWriteOnlyRef, Write as _, WriteOnlyRef};
+}
